429c5274155526a132a5a6a35df389b2cafd314e
hwclass <harderworking.class@gmail.com>
2024-04-17 12:46:06 +0200

    initial commit

---
+++ new/src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Barış Güler';
+export const SITE_DESCRIPTION = 'For the Best Mentorship, Consultancy & Courses';
